7b9f6031ddb87561f7c593fc173a3874ebf190d9
Vivek Chaudhary <vivek2020singh2020@gmail.com>
2025-08-30 17:05:24 +0530

    Lecture-8 of CRUD with db

---
+++ Lecture-8/CRUD_db/models/Neta.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const netaSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim : true,
+        required: true,
+    },
+    party: String,
+    isMale: Boolean,
+    isCorrupt: Boolean
+});
+
+const NetaModel = mongoose.model('Neta' , netaSchema);
+
+module.exports = NetaModel;
